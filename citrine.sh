b7fb0bca3601b8fa9963d9fbb7df5a8969e9cebd
jnats <jnats@salyut.one>
2021-05-14 22:42:51 +0100

    fixed logging

--- citrine.sh
+++ citrine.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo citrine.internal 2>&1 | sudo tee /var/log/citrine.log
+sudo script -O /var/log/citrine.log -q -c "citrine.internal"
 sudo cp /var/log/citrine.log /mnt/var/.
 echo "Run 'reboot' to restart. :)"
