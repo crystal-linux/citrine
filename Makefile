8f34ba8977a80ab4b6abcc6ecbe06db4e3618627
Matt C <mdc028@bucknell.edu>
2021-10-20 14:33:24 -0400

    hhngn fuck make

--- Makefile
+++
@@ -1,7 +0,0 @@
-install:
-	chmod +x *.sh
-	cp *.sh /usr/bin
-	mv /usr/bin/citrine.sh /usr/bin/citrine
-	mv /usr/bin/citrine.internal.sh /usr/bin/citrine.internal
-test: install
-	citrine
---
+++ make.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ins() {
+    chmod +x *.sh
+    cp *.sh /usr/bin
+    mv /usr/bin/citrine.sh /usr/bin/citrine
+    mv /usr/bin/citrine.internal.sh /usr/bin/citrine.internal
+}
+
+testc() {
+    citrine
+}
+
+if [[ "$1" == "" ]]; then
+    echo "./make.sh install - installs citrine"
+    echo "./make.sh test - (installs) then runs citrine"
+    exit 1
+fi
+
+if [[ "$1" == "install" ]]; then
+    ins
+elif [[ "$1" == "test" ]]; then
+    ins
+    testc
+fi
